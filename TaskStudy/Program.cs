79c345ae6e25c124b1674c5eab4a7d4cc2e58faf
hsytkm <t_hosoya1214@yahoo.co.jp>
2019-09-07 14:07:08 +0900

    プロジェクト ファイルを追加します。

---
+++ TaskStudy/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TaskStudy
+{
+    class Program
+    {
+        //https://www.kekyo.net/2016/12/06/6186
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine($"Start");
+
+            var cls1 = new SomeClass1();
+            Console.WriteLine($"Val1={cls1.Val}");
+            Console.WriteLine($"Val1={cls1.Val}");
+            Console.WriteLine($"Val1={cls1.Val}");
+
+
+            var cls2 = new SomeClass2();
+            Console.WriteLine($"Val2={cls2.Val}");
+            Console.WriteLine($"Val2={cls2.Val}");
+            Console.WriteLine($"Val2={cls2.Val}");
+
+            var cls3 = new SomeClass3();
+            Console.WriteLine($"Val3={cls3.Val}");
+            Console.WriteLine($"Val3={cls3.Val}");
+            Console.WriteLine($"Val3={cls3.Val}");
+
+            var cls4 = new SomeClass4();
+            Console.WriteLine($"Val4={cls4.Val}");
+            Console.WriteLine($"Val4={cls4.Val}");
+            Console.WriteLine($"Val4={cls4.Val}");
+
+        }
+    }
+}
---
+++ TaskStudy/SomeClass.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Threading.Tasks;
+using ValueTaskSupplement;
+
+namespace TaskStudy
+{
+    class SomeClass1
+    {
+        public int Val => _val.Result;
+        private ValueTask<int> _val => HeavyMethod();
+
+        private static async ValueTask<int> HeavyMethod()
+        {
+            Console.WriteLine("StartWait1");
+            await Task.Delay(1000);
+            return 1;
+        }
+    }
+
+    class SomeClass2
+    {
+        public int Val => _val;
+        private readonly static int _val = HeavyMethod().Result;
+
+        private static async ValueTask<int> HeavyMethod()
+        {
+            Console.WriteLine("StartWait2");
+            await Task.Delay(2000);
+            return 2;
+        }
+    }
+
+    class SomeClass3
+    {
+        public int Val => _val.Result;
+        private readonly ValueTask<int> _val = ValueTaskEx.Lazy(async () =>
+        {
+            Console.WriteLine("StartWait3");
+            await Task.Delay(2000);
+            return 3;
+        });
+    }
+
+    class SomeClass4
+    {
+        private int _val;
+
+        public int Val
+        {
+            get
+            {
+                while (!taskFinished)
+                {
+                    WaitAsync().Wait();
+                }
+                return _val;
+            }
+        }
+
+        private readonly Task myTask;
+        private bool taskFinished;
+
+        public SomeClass4()
+        {
+            myTask = HeavyMethod().ContinueWith(t =>
+            {
+                Console.WriteLine("Waiting Complete");
+                taskFinished = true;
+            });
+        }
+
+        private async Task HeavyMethod()
+        {
+            Console.WriteLine("StartWait4");
+            await Task.Delay(2000);
+            _val = 4;
+        }
+
+        private async Task WaitAsync()
+        {
+            Console.Write("w");
+            await Task.Delay(500);
+        }
+
+    }
+
+}
